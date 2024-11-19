efdc1129742f03e11d0d5105db5b4f0acfc9ae52
Zandee <uiuxzjchizole@gmail.com>
2024-11-19 23:05:18 +0800

    New

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,8 @@ if (!isset($_SESSION['instructor_id'])) {
 }
 ?>
 
+<!-- HTML AREA -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,20 +15,77 @@ if (!isset($_SESSION['instructor_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard/port.8080</title>
     <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
+
 <body>
-    <h2>Welcome, Instructor!</h2>
-    <ul>
-    <li><a href="create_class.php">Create a New Class</a></li>
-        <li><a href="add_student.php">Add Student to Class</a></li>
-        <li><a href="grade_students.php">Grade Students</a></li>
-        <li><a href="subjects.php">Subjects</a></li>
-        <li><a href="student_managment.php">Student Management</a></li>
-        <li><a href="student_programs.php">Student Programs</a></li>
-        <!-- <li><a href="view_class.php">Class List</a></li> -->
-        <li><a href="grades.php">Grades</a></li>
-        <li><a href="settings.php">Settings</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    </ul>
+   
+<div class="d-flex align-items-center bg-primary 
+"style="height: 100vh ; width: 230px;" >
+     <ul style="list-style: none;"> 
+         <button type="button" class="btn btn-primary">
+         <a class="text-decoration-none text-white" href="create_class.php">Create a New Class</a>
+         </button>
+
+         <button type="button" class="btn btn-primary">
+         <a class="text-decoration-none text-white" href="add_student.php">Add Student</a>
+         </button>
+
+         <button type="button" class="btn btn-primary">
+         <a class="text-decoration-none text-white" href="grade_students.php">Student Grades</a>
+         </button>
+
+         <button type="button" class="btn btn-primary">
+         <a class="text-decoration-none text-white" href="subjects.php">Subject</a>
+         </button>
+
+         <button type="button" class="btn btn-primary">
+         <a class="text-decoration-none text-white" href="student_managment.php">Student Management</a>
+         </button>
+
+         <button type="button" class="btn btn-primary">
+         <a class="text-decoration-none text-white" href="student_programs.php">Student Programs</a>
+         </button>
+
+         <button type="button" class="btn btn-primary">
+         <a class="text-decoration-none text-white" href="view_class.php">Class List</a>
+         </button><br>
+        
+         <button type="button" class="btn btn-primary">
+         <a class="text-decoration-none text-white" href="grades.php">Grades</a>
+         </button><br>
+
+         <button type="button" class="btn btn-primary">
+         <a class="text-decoration-none text-white" href="settings.php">Settings</a>
+         </button><br>
+
+         <button type="button" class="btn btn-primary">
+         <a class="text-decoration-none text-white" href="logout.php">Logout</a>
+         </button>
+    </ul> 
+    <div class="position-relative">
+        <div class="position-absolute top-0 start-0">
+        <h1>Hello Admin</h1>
+        </div>
+    </div>
+    
+</div>
+
+
+    <style>
+       .hover-effect a:hover {
+            color: #7bffc7 !important;
+            width: 200px;
+            height: 30px;
+            background-color: white;
+            display: flex;
+
+        }
+    </style>
+    
+    <!-- Bootstrap Script Modal Ver.. -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
